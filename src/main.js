30dd943170fdc672914afbab2b51a46690ae8100
Mie Nishizawa <mie.gcpn@gmail.com>
2021-05-26 22:31:53 +0900

    CLEAN: remove some dead code

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import ClickOutside from 'vue-click-outside'
+Vue.directive('click-outside', ClickOutside)
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(fasHeart, farHeart);
+library.add(fasHeart, farHeart, faBars);
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
