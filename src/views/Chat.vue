5178c74312f81b9fc3b4ff8ce015979559590317
Mie Nishizawa <mie.gcpn@gmail.com>
2021-06-04 18:48:18 +0900

    ADD: Userinfo component & Modal for unread

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -3,53 +3,83 @@
     <div class="room-list">
       <RoomList ref="roomList"></RoomList>
     </div>
-  <div class="chat-room">
-    <div class="roomname">
-      {{ roomname }}
-      <span v-click-outside="hideMenu" @click="openMenu" class="humberger">
-        <font-awesome-icon :icon="['fas', 'bars']"/>
-      </span>
-        <span v-if="isOpen" class="room-leave" @click="leaveRoom(id)">Leave the room</span>
-    </div>
-    <textarea
-      class="chat-form"
-      type="text"
-      @keypress.prevent.enter.exact="createChat"
-      v-model="newChat"
-      v-focus
-    ></textarea>
 
-    <div class="chats" ref="scrollArea">
-      <div
-        v-for="(chat, id) in chats"
-        :key="chat.id"
-        :class="{
-          'chat-item-own': chat.userId === userId,
-          'chat-item': chat.userId !== userId,
-        }"
-      >
-        <template v-if="chat.userId === userId">
-          <div class="chat-own">
+    <div class="chat-room">
+      <div class="roomname">
+        <router-link :to="{ name: 'Roominfo', params: { id: id } }">
+          {{ roomname }} {{ '(' + joinIds + ')' }}
+        </router-link>
+        <span
+          v-click-outside="hideRoomMenu"
+          @click="openRoomMenu"
+          class="humberger"
+        >
+          <font-awesome-icon :icon="['fas', 'bars']" />
+        </span>
+        <span v-if="isRoommenuOpen" class="humberger-open" @click="leaveRoom(id)">
+          Leave the room
+        </span>
+      </div>
+
+      <UnreadModal v-if="modal">
+        <p>{{ this.unreadMessage }}of Unread</p>
+      </UnreadModal>
+
+      <textarea
+        class="chat-form"
+        type="text"
+        @keypress.prevent.enter.exact="createChat"
+        v-model="newChat"
+        v-focus
+      />
+      <div class="chats" ref="scrollArea">
+        <div
+          v-for="(chat, id) in chats"
+          :key="chat.id"
+          :class="{
+            'chat-item-own': chat.userId === userId,
+            'chat-item': chat.userId !== userId,
+          }"
+        >
+          <template v-if="chat.userId === userId">
             <button @click="deleteChat(id)" class="chat-delete">x</button>
             <span v-if="chat.isRead === true" class="is-read">
-              <p>既読 {{ chat.isReadUsers.length }}</p>
+              <p>Read {{ chat.isReadUsers.length }}</p>
             </span>
             <span class="chat-message-own">{{ chat.item }}</span>
-          </div>
-        </template>
+            <div class="chat-avatar-own">
+              <img 
+                @click="appearUserInfo(id)"
+                v-click-outside="disappearUserInfo" 
+                :src="chat.avatar"
+              />
+            </div>
+          </template>
 
-        <template v-else>
-          <span class="username">{{ chat.username }}</span>
-          <span class="chat-message">{{ chat.item }}</span>
-          <button @click="pushLike(id)">
-            <font-awesome-icon class="chat-nolike" :icon="['far', 'heart']"/>
-          </button>
-          <span class="like-counter">{{ chat.count }}</span>
-          <span class="created-time">{{ getTime(chat) }}</span>
-        </template>
+          <template v-else>
+            <div class="column">
+              <span class="username">{{ chat.username }}</span>
+              <div class="chat-avatar">
+                <img 
+                @click="appearUserInfo(id)"
+                v-click-outside="disappearUserInfo" 
+                :src="chat.avatar"
+              />
+              </div>
+            </div>
+            <span class="chat-message">{{ chat.item }}</span>
+            <button @click="pushLike(id)" class="like">
+              <font-awesome-icon :icon="['far', 'heart']" />
+            </button>
+            <span class="like-counter">{{ chat.likeCount }}</span>
+            <span class="created-time">{{ getTime(chat) }}</span>
+          </template>
+        </div>
       </div>
+
+      <UserInfo v-show="userInfo" ref="userInfo" :roomId="roomId" :chatId="chatId"/>
+
     </div>
-  </div>
   </div>
 </template>
 
@@ -58,45 +88,63 @@ import firebase from "../firebase";
 const db = firebase.firestore();
 import { mapGetters } from "vuex";
 
-import RoomList from '../components/RoomList.vue'
+import RoomList from "../components/RoomList.vue";
+import UserInfo from "../components/UserInfo.vue";
+import UnreadModal from "../components/UnreadModal.vue";
 
 export default {
   name: "Chat",
   props: ["id"],
-  data: function () {
+  data: function() {
     return {
       chats: [],
       chatsRef: null,
       likesRef: null,
       newChat: "",
+      roomId: "",
       userId: "",
+      userAvatar: null,
+      userInfo: false,
       roomname: "",
-      isOpen: false,
+      joinIds: null,
+      isRoommenuOpen: false,
+      isChatmenuOpen: false,
+      avatar: null,
+      unreadMessage: null,
+      modal: false,
+      chatId: null,
     };
   },
   components: {
-    'RoomList': RoomList,
+    RoomList: RoomList,
+    UnreadModal: UnreadModal,
+    UserInfo: UserInfo,
   },
   directives: {
     focus: {
-      inserted: function (el) {
+      inserted: function(el) {
         el.focus();
       },
     },
   },
   computed: {
-    ...mapGetters([ "getUser" ]),
+    ...mapGetters(["getUser"]),
 
     getTime() {
       return (chat) => {
-        return chat.created.toDate().getHours() + ":" + ("0" + chat.created.toDate().getMinutes()).slice(-2)
-      }
+        return (
+          chat.created.toDate().getHours() + ":" + ("0" + chat.created.toDate().getMinutes()).slice(-2)
+        );
+      };
     },
   },
   created() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.userId = user.uid;
+        this.userAvatar = user.photoURL;
+        this.setJoinUsers();
+        console.log(this.getUser)
       } else {
         this.$router.push("/signup");
       }
@@ -104,14 +152,16 @@ export default {
   },
   mounted() {
     this.loadChat();
+    console.log(this.userInfo)
   },
   updated() {
     this.scrollBottom();
   },
   watch: {
-    id(newId, oldId) {
-      console.log(newId, oldId);
+    id() {
       this.loadChat();
+      this.setJoinUsers();
+      console.log("b");
     },
   },
   methods: {
@@ -123,6 +173,7 @@ export default {
           item: this.newChat,
           created: firebase.firestore.FieldValue.serverTimestamp(),
           isRead: false,
+          avatar: this.userAvatar,
         });
         db.collection("rooms").doc(this.id).update({
           updated: firebase.firestore.FieldValue.serverTimestamp(),
@@ -133,107 +184,166 @@ export default {
       }
       this.newChat = "";
       this.scrollBottom();
+      this.updateUnread();
+    },
+    async updateUnread() {
+      let batch = db.batch();
+      const joinUsers = await db.collection("rooms").doc(this.id).collection("joinUsers")
+        .where(firebase.firestore.FieldPath.documentId(), "!=", this.getUser._id).get();
+      joinUsers.docs.forEach((doc, index) => {
+        if ((index + 1) % 500 === 0) {
+          batch.commit();
+          batch = db.batch();
+        }
+        batch.update(doc.ref, {
+          unreadMessage: firebase.firestore.FieldValue.increment(1),
+        });
+      });
+      batch.commit();
     },
     scrollBottom() {
       this.$nextTick(() => {
-        const chatLog = this.$refs.scrollArea
-        if (!chatLog) return
-        chatLog.scrollTop = chatLog.scrollHeight
-      })
+        const chatLog = this.$refs.scrollArea;
+        if (!chatLog) return;
+        chatLog.scrollTop = chatLog.scrollHeight;
+      });
     },
     loadChat() {
       this.chatsRef = db.collection("rooms").doc(this.id).collection("chats");
       this.chatsRef.orderBy("created", "asc").onSnapshot((querySnapshot) => {
-        let obj = {}
+        let obj = {};
         querySnapshot.docs.map((doc) => {
           obj[doc.id] = doc.data();
         });
         this.chats = obj;
       });
       db.collection("rooms").doc(this.id).get().then((doc) => {
-      this.roomname = doc.data().roomname
-      })
-      this.openChat();
+        this.roomname = doc.data().roomname;
+        this.joinIds = doc.data().joinIds.length;
+      });
+      this.updateRead();
     },
-    deleteChat(id) {
-      this.chatsRef.doc(id).delete()
-    },
-    async openChat() {
+    async updateRead() {
       let batch = db.batch();
-      const chats = await db.collection("rooms").doc(this.id).collection("chats").where("userId", "!=", this.getUser._id).get();
+      const chats = await db.collection("rooms").doc(this.id).collection("chats")
+      .where("userId", "!=", this.getUser._id).get();
       chats.docs.forEach((doc, index) => {
         if ((index + 1) % 500 === 0) {
-        batch.commit(); 
-        batch = db.batch(); 
+          batch.commit();
+          batch = db.batch();
         }
         batch.update(doc.ref, {
-          'isRead': true,
-          'isReadUsers': firebase.firestore.FieldValue.arrayUnion(this.getUser._id),
+          isRead: true,
+          isReadUsers: firebase.firestore.FieldValue.arrayUnion(this.getUser._id),
         });
       });
       batch.commit();
-      this.joinChat();
     },
-    joinChat() {
+
+    setJoinUsers() {
       db.collection("rooms").doc(this.id).update({
-        joinUsers: firebase.firestore.FieldValue.arrayUnion(this.userId),
-      })
+        joinIds: firebase.firestore.FieldValue.arrayUnion(this.getUser._id),
+      });
+      db.collection("rooms").doc(this.id).collection("joinUsers").doc(this.getUser._id).set({
+        userId: this.getUser._id,
+        username: this.getUser.username,
+      },{ merge: true }
+      );
+      this.getUnread();
     },
+    getUnread() {
+      db.collection("rooms").doc(this.id).collection("joinUsers").doc(this.getUser._id)
+      .get().then((doc) => {
+        this.unreadMessage = doc.data().unreadMessage;
+        if (this.unreadMessage > 0) {
+          this.modal = true;
+          setTimeout(this.resetUnread, 2000);
+        }
+      });
+    },
+    resetUnread() {
+      db.collection("rooms").doc(this.id).collection("joinUsers").doc(this.getUser._id).update({
+        unreadMessage: 0,
+      });
+      this.modal = false;
+    },
+
     pushLike(id) {
       this.chatsRef.doc(id).collection("likedUsers").doc(this.userId).set({
         username: this.getUser.username,
       });
       this.chatsRef.doc(id).update({
-        count: firebase.firestore.FieldValue.increment(1)
+        likeCount: firebase.firestore.FieldValue.increment(1),
       });
     },
+
     async leaveRoom() {
       await db.collection("rooms").doc(this.id).update({
-        joinUsers: firebase.firestore.FieldValue.arrayRemove(this.getUser._id)
-      })
+        joinIds: firebase.firestore.FieldValue.arrayRemove(this.getUser._id),
+      });
+      db.collection("rooms").doc(this.id).collection("joinUsers").doc(this.getUser._id).delete();
       this.chats = null;
       this.roomname = null;
     },
-    openMenu() {
-      this.isOpen = true;
+
+    deleteChat(id) {
+      this.chatsRef.doc(id).delete();
     },
-    hideMenu() {
-      this.isOpen = false;
-    }
-  }
-}
+
+    onEnterKey(e) {
+      console.log("ClickedEnter", e);
+    },
+
+    openRoomMenu() {
+      this.isRoommenuOpen = true;
+    },
+    hideRoomMenu() {
+      this.isRoommenuOpen = false;
+    },
+    openChatMenu() {
+      this.isChatmenuOpen = true;
+    },
+    hideChatMenu() {
+      this.isChatmenuOpen = false;
+    },
+    appearUserInfo(id) {
+      this.roomId = this.id;
+      this.chatId = id;
+      // this.$refs.userInfo.loadUserInfo();
+    },
+    disappearUserInfo() {
+      this.userInfo = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
 #chat {
   height: 100vh;
-  background: #FFF
+  background: #fff;
 }
 .chat-room {
   padding-top: 3.5em;
-  width: 100% - 250px;
-  margin: 0 0 0 250px;
 }
 .room-list {
-  float: left;
-  width: 250px;
-  height: 100%;
-  padding-top: 3.5em;
+  display: none;
 }
 .roomname {
   width: 100%;
-  padding: 0.6em;
+  height: 3em;
+  padding: 0.5em 0.5em 0.6em 0;
   margin-bottom: 1em;
   font-weight: bold;
   text-align: right;
-  background: #F3F4F6
+  background: #f3f4f6;
 }
 .chats {
   height: calc(100vh - 242px);
   overflow-y: auto;
 }
 button {
-  background-color: rgba(0,0,255,0);
+  background-color: rgba(0, 0, 255, 0);
   border: none;
 }
 .chat-form {
@@ -242,16 +352,19 @@ button {
   padding: 10px 15px;
   height: 120px;
   width: 100%;
-  background: #FFF;
+  background: #fff;
   font-size: 1em;
-  border: solid 1px #E9B5B4;
+  border: solid 1px #e9b5b4;
 }
 .chat-item {
-  text-align: left;
+  display: flex;
+  align-items: center;
   margin: 0 0 0.7em 2em;
 }
 .chat-item-own {
-  text-align: right;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
   margin: 0 2em 0.7em 0;
 }
 .chat-message,
@@ -261,7 +374,7 @@ button {
   padding: 0.6em;
   border-radius: 20px;
   line-height: 1.1em;
-  background: #EFEFEF;
+  background: #efefef;
   max-width: 75%;
 }
 .chat-message-own {
@@ -274,7 +387,7 @@ button {
   left: -12px;
   bottom: 12px;
   border: 3px solid transparent;
-  border-right: 12px solid #EFEFEF;
+  border-right: 12px solid #efefef;
 }
 .chat-message-own::after {
   position: absolute;
@@ -285,11 +398,6 @@ button {
   border: 3px solid transparent;
   border-left: 12px solid #deefe8;
 }
-.chat-own {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-}
 .chat-delete {
   margin-left: 1.5em;
   padding: 0 0.2em;
@@ -298,27 +406,32 @@ button {
 }
 .username {
   font-size: 0.7em;
+  margin-right: 1.2em;
 }
-.like-counter, .is-read p, .created-time{
+.like-counter,
+.is-read p,
+.created-time {
   font-size: 0.7em;
-  color: #5F6062;
+  color: #5f6062;
 }
 .username {
   display: block;
 }
 .is-read {
   margin: 0.3em;
-  color: #5F6062;
+  color: #5f6062;
 }
-.created-time{
+.like {
+  cursor: pointer;
+}
+.created-time {
   margin-left: 0.5em;
 }
 .humberger {
   font-size: 1.3em;
   padding: 0.6em 1em;
-  position: relative;
 }
-.room-leave {
+.humberger-open {
   position: absolute;
   height: 45px;
   top: 100px;
@@ -328,16 +441,72 @@ button {
   color: white;
   display: block;
   padding: 0.6em;
-  background: rgba(0,0,0,0.75);
-  z-index: 2;
+  background: rgba(0, 0, 0, 0.75);
+  z-index: 5;
 }
-.room-leave:after{
+.humberger-open:after {
   position: absolute;
-  border-bottom: 12px solid rgba(0,0,0,0.75);
-  border-left: 10px solid transparent;   
-  border-right: 10px solid transparent;  
+  border-bottom: 12px solid rgba(0, 0, 0, 0.75);
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
   top: -12px;
   right: 8px;
   content: "";
+}
+.chat-avatar-own {
+  margin-left: 1em;
+}
+.chat-avatar {
+  margin-right: 0.6em;
+}
+.chat-avatar-own img,
+.chat-avatar img {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  object-fit: cover;
+  cursor: pointer;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+}
+@media (min-width: 600px) {
+  .pref-roomnames,
+  .room-roomnames,
+  .my-roomnames {
+    width: 500px;
+  }
+  .room-list {
+    display: block;
+    float: left;
+    width: 250px;
+    height: 100%;
+    padding-top: 3.5em;
+  }
+  .chat-room {
+    padding-top: 3.5em;
+    width: 100% - 250px;
+    margin: 0 0 0 250px;
+  }
+}
+@media (min-width: 1025px) {
+  .pref-roomnames,
+  .room-roomnames,
+  .my-roomnames {
+    width: 500px;
+  }
+  .room-list {
+    display: block;
+    float: left;
+    width: 250px;
+    height: 100%;
+    padding-top: 3.5em;
+  }
+  .chat-room {
+    padding-top: 3.5em;
+    width: 100% - 250px;
+    margin: 0 0 0 250px;
+  }
 }
 </style>
