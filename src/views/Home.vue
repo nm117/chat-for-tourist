bd6ff0cc1790be1842c9714c8b60c9b040f4bc8f
Mie Nishizawa <mie.gcpn@gmail.com>
2021-06-07 17:19:25 +0900

    CHANGE: Design of top page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,41 @@
 <template>
   <div id="home">
     <div class="top-bg">
-      <h1>Title Title Title<br>
-          Title Title Title</h1>
+      <h1>Let's get more right information for sightseeing</h1>
     </div>
 
-    <h2>Unforgettable memories to every tourist</h2>
+    <h2>We support you to</h2>
 
     <div class="abouts">
-      <div class="about"><img src="@/assets/about-1.jpg" class="sub-photo" alt="Kaimondake">
-      <h3>DISCOVER</h3>
-      <p>sub sub sub sub sub sub</p></div>
-      <div class="about"><img src="@/assets/about-2.jpg" class="sub-photo" alt="Nagasaki">
-      <h3>INTERACT</h3>
-      <p>sub sub sub sub sub sub</p></div>
-      <div class="about"><img src="@/assets/about-3.jpg" class="sub-photo" alt="Oboke">
-      <h3>FAIR</h3>
-      <p>sub sub sub sub sub sub</p></div>
-      <div class="about"><img src="@/assets/about-4.jpg" class="sub-photo" alt="Zao">
-      <h3>UNIQUE</h3>
-      <p>sub sub sub sub sub sub</p></div>
+      <div class="about">
+        <img src="@/assets/about-1.jpg" class="sub-photo" alt="Kaimondake" />
+        <h3>GET</h3>
+        <p>fair information and price</p>
+      </div>
+      <div class="about">
+        <img src="@/assets/about-2.jpg" class="sub-photo" alt="Nagasaki" />
+        <h3>INTERACT</h3>
+        <p>with locals would be the best memories in your trip</p>
+      </div>
+      <div class="about">
+        <img src="@/assets/about-3.jpg" class="sub-photo" alt="Oboke" />
+        <h3>DISCOVER</h3>
+        <p>hired tourist spots and views</p>
+      </div>
+      <div class="about">
+        <img src="@/assets/about-4.jpg" class="sub-photo" alt="Zao" />
+        <h3>FIND</h3>
+        <p>unique places not found in guidebooks</p>
+      </div>
     </div>
-
   </div>
 </template>
 
 <style scoped>
 .top-bg {
-  background-color: #66BAB7;
+  background-color: #66bab7;
   padding: 0px;
-  height: 20em; 
+  height: 20em;
   margin: -3em 0 3em 0;
   width: 100%;
 }
@@ -37,12 +43,12 @@ h1 {
   color: white;
   font-style: italic;
   font-size: 2em;
-  padding: 100px 0 0 60px
+  padding: 100px 0 0 60px;
 }
 h2 {
-  font-size: 1.3em;
-  text-align: center;
-  margin-bottom: 1em;
+  text-align: left;
+  margin: 0 0 1em 5em;
+  color: gray;
 }
 h3 {
   font-size: 1em;
@@ -50,6 +56,11 @@ h3 {
 }
 p {
   font-size: 1em;
+}
+hr {
+  color: gray;
+  margin-bottom: 1em;
+  width: 85%;
 }
 .abouts {
   padding: 0 2%;
@@ -66,21 +77,14 @@ p {
     margin: 0 1em;
     max-width: 40%;
   }
-  h2 {
-    font-size: 1.5em;
-  }
 }
 @media (min-width: 1025px) {
   .about {
     margin: 0 1em;
     max-width: 20%;
   }
-  h2 {
-    font-size: 1.5em;
-  }
 }
 .sub-photo {
   width: 95%;
-  text-align: center;
 }
 </style>
